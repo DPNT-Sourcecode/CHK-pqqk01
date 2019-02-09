0f5d87d1d4c777ec3c40419f4f5475fb79fc8dd1
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-02-09 18:55:08 +0000

    Sat Feb 09 18:55:08 GMT 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,18 +7,21 @@ def checkout(skus):
 	total = 0
 	for i in skus:
 		if(i not in "ABCD"):
-			print("wrong")
-		if i == 'A':
-			items[0] = items[0] + 1
-		elif i == 'B':
-			items[1] = items[1] + 1
-		elif i == 'C':
-			items[2] = items[2] + 1
-		elif i == 'D':
-			items[3] = items[3] + 1
+			return -1
+		else:
+			if i == 'A':
+				items[0] = items[0] + 1
+			elif i == 'B':
+				items[1] = items[1] + 1
+			elif i == 'C':
+				items[2] = items[2] + 1
+			elif i == 'D':
+				items[3] = items[3] + 1
 	
+	for i in items:
+		print (i)
 
     # raise NotImplementedError()
 
 
-checkout("EAABACDBBAD")
+checkout("AABACDBBAD")
