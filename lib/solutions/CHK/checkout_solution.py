503768e285c00e020c2d0a6c930cdf5e4e96a2ea
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-02-09 18:52:08 +0000

    Sat Feb 09 18:52:08 GMT 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,9 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+	items = [0] * 4
+	total = 0
 	for i in skus:
-		print(i)
+		if(i not in "ABCD"):
+			print("wrong")
+		if i == 'A':
+			items[0] = items[0] + 1
+		elif i == 'B':
+			items[1] = items[1] + 1
+		elif i == 'C':
+			items[2] = items[2] + 1
+		elif i == 'D':
+			items[3] = items[3] + 1
+	
+
     # raise NotImplementedError()
 
 
-checkout("AABACDBBAD")
+checkout("EAABACDBBAD")
