d7166124d67e5dd58763d6023cfe7a16204dafaf
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-02-09 20:55:03 +0000

    Sat Feb 09 20:55:03 GMT 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,27 +1,46 @@
-
-
-# noinspection PyUnusedLocal
-# skus = unicode string
-def checkout(skus):
-	items = [0] * 4
+#Calculates the total value from the basket taking into account the special offers
+def repeat(i,val):
 	total = 0
+	if i == 'A':
+		if(val>=3):
+			count = 0	
+			while (val >= 3):
+				val = val - 3
+				count = count + 1
+			total = total + ( count * 130 + 50 * val)
+		else:
+			total = total + 50 * val
+	elif i == 'B':
+		if(val>=2):
+			count = 0	
+			while (val >= 2):
+				val = val - 2
+				count = count + 1
+			total = total + ( count * 45 + 30 * val)
+		else:
+			total = total + 30 * val
+	elif i == 'C':
+		total = total + 20 * val
+	elif i == 'D':
+		total = total + 15 * val
+	return int(total) #returning the total of a specific product
+				
+def checkout(skus):
+	needToPay =0 # Initializing the final amout needed to be paid
+	items = {'A':0, 'B':0, 'C':0, 'D':0} # Intitialized the dictionary of the stocks
 	for i in skus:
 		if(i not in "ABCD"):
 			return -1
 		else:
 			if i == 'A':
-				items[0] = items[0] + 1
+				items['A'] = items['A'] + 1
 			elif i == 'B':
-				items[1] = items[1] + 1
+				items['B'] = items['B'] + 1
 			elif i == 'C':
-				items[2] = items[2] + 1
+				items['C'] = items['C'] + 1
 			elif i == 'D':
-				items[3] = items[3] + 1
+				items['D'] = items['D'] + 1
 	
-	for i in items:
-		print (i)
-
-    # raise NotImplementedError()
-
-
-checkout("AABACDBBAD")
+	for i in items.keys():
+		needToPay = needToPay + repeat(i,items[i])
+	return (needToPay)
