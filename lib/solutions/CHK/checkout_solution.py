f09dba6be9e38cc0bfb3bf8a229b2fae6f19d506
sbx_user1060 <sbx_user1060@ip-10-128-7-244.eu-west-2.compute.internal>
2019-02-10 17:43:56 +0000

    Sun Feb 10 17:43:56 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,4 @@
-#Applying the discounts
+# Applying the discounts
 def specialOffers(i,val,discount,items):
 	if i == 'A':
 		if(val>=5):
@@ -15,12 +15,10 @@ def specialOffers(i,val,discount,items):
 			while aux >= 2:
 				aux = aux -2
 				val = val -1
-				# items['B'] = items['B'] - 1
 				discount = discount - 30 
 		if(val>=2):
 			while (val >= 2):
 				val = val - 2
-				# items['B'] = items['B'] - 2
 				discount = discount - 15
 	elif i == 'F':
 		if val>=3:
@@ -40,7 +38,6 @@ def specialOffers(i,val,discount,items):
 		if(val>=2):
 			while (val >= 2):
 				val = val - 2
-				# items['B'] = items['B'] - 2
 				discount = discount - 20
 	elif i == 'N':
 		if(val>=3 and items['M']!=0):
@@ -61,20 +58,11 @@ def specialOffers(i,val,discount,items):
 			while aux >= 3:
 				aux = aux - 3
 				val = val - 1
-				# items['B'] = items['B'] - 1
 				discount = discount - 30 
 		if(val>=3):
 			while (val >= 3):
 				val = val - 3
-				# items['B'] = items['B'] - 2
 				discount = discount - 10
-
-		# if(val>=3):
-		# 	count = 0
-		# 	while (val >= 3):
-		# 		val = val -3
-		# 		count = count + 1
-		# 	discount = discount - ( count * 10)
 	elif i == 'U':
 		if val>=4:
 			while val >= 4:
@@ -89,16 +77,51 @@ def specialOffers(i,val,discount,items):
 			discount = discount - ( count * 20)
 		if(val>=2):
 			discount = discount - 10
-
-	# 	if(val==1):
-	# 		items['B'] = items['B'] - 1
-	# elif i == 'C':
-	# 	items['C'] = 0 # OR items['C'] - val
-	# elif i == 'D':
-	# 	items['D'] = 0 # OR items['D'] - val
-	# elif i == 'E':
-	# 	items['E'] = 0 # OR items['E'] - val
 	return int(discount)
+
+# Offer discounts when grouping STXYZ products 
+def groupProducts(needToPay,items):
+	STY = items['S'] + items['T'] + items['Y']
+	X = items['X']
+	Z = items['Z']
+	while (STY + X + Z >= 3):
+		if Z >= 3:
+			while(Z >= 3):
+				Z = Z - 3
+				needToPay = needToPay - 18
+		if Z == 2 and STY!=0:
+			Z = Z - 2
+			STY = STY -1 
+			needToPay = needToPay - 17
+		if Z == 1 and STY>1:
+			Z = Z - 1
+			STY = STY - 2
+			needToPay = needToPay - 16
+		if Z == 2 and X!=0:
+			Z = Z - 2
+			X = X - 1
+			needToPay = needToPay - 14
+		if Z == 1 and X>1:
+			Z = Z - 1
+			X = X - 2
+			needToPay = needToPay - 10
+		if STY >= 3: 
+			while STY >=3:
+				STY = STY -3
+				needToPay = needToPay - 15	
+		if STY == 2 and X!=0:
+			STY = STY - 2
+			X = X - 1
+			needToPay = needToPay - 12
+		if STY == 1 and X>1:
+			STY = STY - 1
+			X = X - 2
+			needToPay = needToPay - 9
+		if X >= 3:
+			while(X >= 3):
+				X = X - 3
+				needToPay = needToPay - 6		
+	return int(needToPay)
 
 #Computes the total without applying any discounts
 def add(i,val,price):
@@ -108,59 +131,22 @@ def add(i,val,price):
 
 def checkout(skus):
 	needToPay = 0 # Initializing the final amout needed to be paid
-	items = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}  # Intitialized the dictionary of the stocks
-	price = {'A':50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G':20, 'H':10, 'I':35, 'J':60, 'K':70, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 'Q':30, 'R':50, 'S':20, 'T':20, 'U':40, 'V':50, 'W':20, 'X':17, 'Y':20, 'Z':21}  # Intitialized the dictionary of the prices
+	# Intitialized the dictionary of the stocks
+	items = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}  
+	# Intitialized the dictionary of the prices
+	price = {'A':50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G':20, 'H':10, 'I':35, 'J':60, 'K':70, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 'Q':30, 'R':50, 'S':20, 'T':20, 'U':40, 'V':50, 'W':20, 'X':17, 'Y':20, 'Z':21}  
 	for i in skus:
 		if(i not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
 			print(-1)
 			return -1
 		else:
 			items[i]=items[i]+1
-	print(items)
+	#print(items)
 	for i in items.keys():
 		needToPay = needToPay + add(i,items[i],price)
 		needToPay = specialOffers(i,items[i],needToPay,items)
-	print(needToPay)
+	needToPay = groupProducts(needToPay,items)
+	#print(needToPay)
 	return (needToPay)
 
-# checkout('ABCDECBAABCABBAAAEEAA')
-# checkout('')
-checkout('KKKK')
-# checkout('B')
-# checkout('C')
-# checkout('D')
-# checkout('E')
-# checkout('a')
-# checkout('-')
-# checkout('ABCa')
-# checkout('AxA')
-# checkout('ABCDE')
-# checkout('A')
-# checkout('AA')
-# checkout('AAA')
-# checkout('AAAA')
-# checkout('AAAAA')
-# checkout('AAAAAA')
-# checkout('AAAAAAA')
-# checkout('AAAAAAAA')
-# checkout('AAAAAAAAA')
-# checkout('AAAAAAAAAA')
-# checkout("EE")
-# checkout("EEB")
-# checkout("EEEB")
-# checkout("EEEEBB")
-# checkout("BEBEEE")
-# checkout("A")
-# checkout("AA")
-# checkout("AAA")
-# checkout("AAAA")
-# checkout("AAAAA")
-# checkout("AAAAAA")
-# checkout("B")
-# checkout("BB")
-# checkout("BBB")
-# checkout("BBBB")
-# checkout("ABCDEABCDE")
-# checkout("CCADDEEBBA")
-# checkout("AAAAAEEBAAABB")
-# checkout("ABCDECBAABCABBAAAEEAA")
+#checkout('AAAAAZZZZSSXTTTZ')
