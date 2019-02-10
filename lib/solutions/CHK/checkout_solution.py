135ccd67bd87d016c741507f2f5e5a5e26e54906
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-02-10 01:18:30 +0000

    Sun Feb 10 01:18:30 GMT 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,30 +1,30 @@
 items = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0} # Intitialized the dictionary of the stocks
-#Calculates the total value from the basket taking into account the special offers
 
+#Applying the discounts
 def specialOffers(i,val,discount):
 	if i == 'A':
-		if val >= 5:
+		if(val>=5):
 			count = 0
-			while val >= 5:
-				val = val - 5
+			while (val >= 5):
+				val = val -5
 				count = count + 1
-			discount = discount - (count * 50)
-		if val >= 3:
+			discount = discount - ( count * 50)
+		if(val>=3):
 			discount = discount - 20
 	elif i == 'B':
-		if items['E']>=2 and val != 0:
+		if items['E']>=2 and val!=0:
 			aux = items['E']
 			while aux >= 2:
-				aux = aux - 2
-				val = val - 1
-				discount = discount - 30
-		if val >= 2:
-			while val >= 2:
+				aux = aux -2
+				val = val -1
+				discount = discount - 30 
+		if(val>=2):
+			while (val >= 2):
 				val = val - 2
 				discount = discount - 15
 	return int(discount)
 
-# Computes the total without applying the discount
+#Computes the total without applying any discounts
 def add(i,val):
 	total = 0
 	if i == 'A':
@@ -38,54 +38,10 @@ def add(i,val):
 	elif i == 'E':
 		total = total + 40 * val
 	return int(total) #returning the total of a specific product
-		
 
-	# 	total = 0
-	# if i == 'A':
-	# 	if(val>=5):
-	# 		count = 0
-	# 		while (val >= 5):
-	# 			val = val -5
-	# 			count = count + 1
-	# 		total = total + ( count * 200)
-	# 	if(val>=3):
-	# 		count = 0	
-	# 		while (val >= 3):
-	# 			val = val - 3
-	# 			count = count + 1
-	# 		total = total + ( count * 130 + 50 * val)
-	# 	else:
-	# 		total = total + 50 * val
-	# elif i == 'B':
-	# 	if(val>=2):
-	# 		count = 0	
-	# 		while (val >= 2):
-	# 			val = val - 2
-	# 			count = count + 1
-	# 		total = total + ( count * 45 + 30 * val)
-	# 	else:
-	# 		total = total + 30 * val
-	# elif i == 'C':
-	# 	total = total + 20 * val
-	# elif i == 'D':
-	# 	total = total + 15 * val
-	# elif i == 'E':
-	# 	if(val>=4): # 45 pounds discount is applied (2 items of B)
-	# 		count45 = 0
-	# 		while val >= 4:
-	# 			val = val - 4
-	# 			count45 = count45 + 1
-	# 		total = total + (count45 * 160 - count45 * 45)
-	# 	if val >= 2: # 30 pounds discount is applied (1 item of B)
-	# 		val = val - 2
-	# 		total = total + 80 - 30
-	# 	if val == 1:
-	# 		total = total + 40
-	# return int(total) #returning the total of a specific product		
 def checkout(skus):
 	needToPay =0 # Initializing the final amout needed to be paid
-
-	#Count how many products are in the basket
+	
 	for i in skus:
 		if(i not in "ABCDE"):
 			return -1
@@ -104,9 +60,9 @@ def checkout(skus):
 		needToPay = needToPay + add(i,items[i])
 
 	for i in items.keys():
-		needToPay = specialOffers(i,items[i],needToPay)
+		needToPay=specialOffers(i,items[i],needToPay)
 	print(needToPay)
 	return (needToPay)
 
-checkout("CCADDEEBBA")
+checkout("EEEBB")
 
