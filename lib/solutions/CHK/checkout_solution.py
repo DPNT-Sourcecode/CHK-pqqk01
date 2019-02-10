73843c814896c8e1f1c0c6426dc2c7cdcb8bac28
sbx_user1060 <sbx_user1060@ip-10-129-140-38.eu-west-2.compute.internal>
2019-02-10 15:53:41 +0000

    Sun Feb 10 15:53:41 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -44,48 +44,38 @@ def specialOffers(i,val,discount,items):
 	return int(discount)
 
 #Computes the total without applying any discounts
-def add(i,val):
+def add(i,val,price):
 	total = 0
-	if i == 'A':
-		total = total + 50 * val
-	elif i == 'B':
-		total = total + 30 * val
-	elif i == 'C':
-		total = total + 20 * val
-	elif i == 'D':
-		total = total + 15 * val
-	elif i == 'E':
-		total = total + 40 * val
-	elif i == 'F':
-		total = total + 10 * val
+	total = total + price['i'] * val
+	# if i == 'A':
+	# 	total = total + 50 * val
+	# elif i == 'B':
+	# 	total = total + 30 * val
+	# elif i == 'C':
+	# 	total = total + 20 * val
+	# elif i == 'D':
+	# 	total = total + 15 * val
+	# elif i == 'E':
+	# 	total = total + 40 * val
+	# elif i == 'F':
+	# 	total = total + 10 * val
 	return int(total) #returning the total of a specific product
 
 def checkout(skus):
 	needToPay = 0 # Initializing the final amout needed to be paid
 	items = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}  # Intitialized the dictionary of the stocks
+	price = {'A':50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G':20, 'H':10, 'I':35, 'J':60, 'K':80, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 'Q':30, 'R':50, 'S':30, 'T':20, 'U':40, 'V':50, 'W':20, 'X':90, 'Y':10, 'Z':50}  # Intitialized the dictionary of the prices
 	for i in skus:
 		if(i not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
 			print(-1)
 			return -1
 		else:
 			items[i]=items[i]+1
-			# if i == 'A':
-			# 	items['A'] = items['A'] + 1
-			# elif i == 'B':
-			# 	items['B'] = items['B'] + 1
-			# elif i == 'C':
-			# 	items['C'] = items['C'] + 1
-			# elif i == 'D':
-			# 	items['D'] = items['D'] + 1
-			# elif i == 'E':
-			# 	items['E'] = items['E'] + 1
-			# elif i == 'F':
-			# 	items['F'] = items['F'] + 1
 	print(items)
 	for i in items.keys():
-		needToPay = needToPay + add(i,items[i])
+		needToPay = needToPay + add(i,items[i],price)
 		needToPay = specialOffers(i,items[i],needToPay,items)
-	# print(needToPay)
+	print(needToPay)
 	return (needToPay)
 
 # checkout('ABCDECBAABCABBAAAEEAA')
@@ -129,5 +119,6 @@ checkout('AAAAAFFF')
 # checkout("CCADDEEBBA")
 # checkout("AAAAAEEBAAABB")
 # checkout("ABCDECBAABCABBAAAEEAA")
+
 
 
