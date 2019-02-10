1a2a63aee7097c8990979969d45e4f85b782fe61
sbx_user1059 <sbx_user1059@ip-10-128-211-85.eu-west-2.compute.internal>
2019-02-10 13:23:23 +0000

    Sun Feb 10 13:23:23 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,28 +3,44 @@ items = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0} # Intitialized the dictionary of the
 #Applying the discounts
 def specialOffers(i,val,discount):
 	if i == 'A':
-		if val >= 5:
+		if(val>=5):
 			count = 0
-			while val >= 5:
-				val = val - 5
+			while (val >= 5):
+				val = val -5
+				items['A'] = items['A'] - 5
 				count = count + 1
-			discount = discount - (count * 50)
-		if val >= 3:
+			discount = discount - ( count * 50)
+		if(val>=3):
 			discount = discount - 20
+			items['A'] = items['A'] - 3
+		if(val==2):
+			items['A'] = items['A'] - 2
+		if(val==1):
+			items['A'] = items['A'] - 1
 	elif i == 'B':
-		if items['E']>=2 and val != 0:
+		if items['E']>=2 and val!=0:
 			aux = items['E']
 			while aux >= 2:
-				aux = aux - 2
-				val = val - 1
-				discount = discount - 30
-		if val >= 2:
-			while val >= 2:
+				aux = aux -2
+				val = val -1
+				items['B'] = items['B'] - 1
+				discount = discount - 30 
+		if(val>=2):
+			while (val >= 2):
 				val = val - 2
+				items['B'] = items['B'] - 2
 				discount = discount - 15
+		if(val==1):
+			items['B'] = items['B'] - 1
+	elif i == 'C':
+		items['C'] = 0 # OR items['C'] - val
+	elif i == 'D':
+		items['D'] = 0 # OR items['D'] - val
+	elif i == 'E':
+		items['E'] = 0 # OR items['E'] - val
 	return int(discount)
 
-# Computes the total without applying the discount
+#Computes the total without applying any discounts
 def add(i,val):
 	total = 0
 	if i == 'A':
@@ -38,11 +54,9 @@ def add(i,val):
 	elif i == 'E':
 		total = total + 40 * val
 	return int(total) #returning the total of a specific product
-	
-def checkout(skus):
-	needToPay =0 # Initializing the final amout needed to be paid
 
-	#Count how many products are in the basket
+def checkout(skus):
+	needToPay = 0 # Initializing the final amout needed to be paid
 	for i in skus:
 		if(i not in "ABCDE"):
 			return -1
@@ -58,10 +72,20 @@ def checkout(skus):
 			elif i == 'E':
 				items['E'] = items['E'] + 1
 	for i in items.keys():
+		# print("need1: ", i,items[i],needToPay)
 		needToPay = needToPay + add(i,items[i])
+		# print("need2: ", i,items[i],needToPay)
+	# for i in items.keys():
+		# print(i,items[i],needToPay)
+		# print("need3: ",i,items[i],needToPay)
 		needToPay = specialOffers(i,items[i],needToPay)
-
-	print(needToPay)
+		# print("need4: ",i,items[i],needToPay)
+	# print(needToPay)
 	return (needToPay)
 
-checkout("BBEE")
+# checkout('')
+# checkout('A')
+# checkout('BB')
+# checkout('C')
+# checkout('D')
+# checkout('ABBE')
